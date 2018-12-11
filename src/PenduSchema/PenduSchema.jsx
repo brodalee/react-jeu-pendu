a8447b1e14ff7465b234dcbbc27c1155438b1082
Brodalee <boubou-28@live.fr>
2018-12-11 16:58:56 +0100

    Brodalee
    Starting to code the react-pendu

---
+++ src/PenduSchema/PenduSchema.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import autoBind from 'react-autobind';
+
+class PenduSchema extends Component{
+    
+    constructor(props){
+        super(props);
+        autoBind(this);
+    }
+    
+    render(){
+        return (<div>azeaze</div>);
+    }
+};
+
+export default PenduSchema;
